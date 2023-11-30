f726cef1d94cedda409748023774e053a5c1ee0a
idealy-ma <91415549+Tokyram@users.noreply.github.com>
2023-11-30 15:15:22 +0300

    projet

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text>Hello hello bonjour</Text>;
+};
+
+export default App;
